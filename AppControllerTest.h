f28e86693d83ba56bdbb6cb360540091a78efa88
mathias.kegelmann@gmail.com <mathias@localhost.local>
2009-08-21 16:45:24 +0200

    Added simple unit test and set up project to run tests before running the executable.

---
+++ AppControllerTest.h
@@ -0,0 +1,16 @@
+//
+//  AppControllerTest.h
+//  Doors
+//
+//  Created by Mathias Kegelmann on 8/21/09.
+//  Copyright 2009 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+#import "AppController.h"
+
+@interface AppControllerTest : SenTestCase {
+    AppController *appController;
+}
+
+@end
---
+++ AppControllerTest.m
@@ -0,0 +1,41 @@
+//
+//  AppControllerTest.m
+//  Doors
+//
+//  Created by Mathias Kegelmann on 8/21/09.
+//  Copyright 2009 __MyCompanyName__. All rights reserved.
+//
+
+#import "AppControllerTest.h"
+
+
+@implementation AppControllerTest
+-(void) setUp
+{
+    appController = [AppController new];
+}
+
+-(void)tearDown
+{
+    [appController release];
+}
+
+-(void) testDoorsAreInitiallyOpen
+{
+    STAssertFalse([appController doorsClosed], nil);
+}
+
+-(void) testStartOpensDoor
+{
+    [appController start:nil];
+    STAssertTrue([appController doorsClosed], nil);  
+}
+
+-(void) testResetClosesDoorAgain
+{
+    [appController start:nil];
+    [appController reset:nil];
+    STAssertFalse([appController doorsClosed], nil);
+}
+
+@end
